94291e2fde6cb4d8a645adf0de214af7df86376b
Rihhadatul Aliyah <rihhadatulaliyah278@gmail.com>
2025-11-30 14:05:02 +0700

    init project sikost

---
+++ app/Models/Profile.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $table = 'profile';
+    protected $primaryKey = 'id_profile';
+    public $timestamps = true; 
+
+    protected $fillable = [
+        'user_id',
+        'nama_lengkap',
+        'alamat',
+        'no_hp',
+        'jenis_kelamin',
+        'tempat_tanggal_lahir',
+        'foto_profile',
+        'foto_ktp',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
